99baf35beebf7f4cd6d785989c80ddc998a766f8
Aneeka <182370815+aneeka026@users.noreply.github.com>
2025-07-16 20:52:38 +0530

    Delete first.js

--- first.js
+++
@@ -1,7 +0,0 @@
-let inp = document.querySelector("input");
-let p = document.querySelector("p");
-
-inp.addEventListener("input",function(){
-    console.log("final value = ",inp.value);   
-    p.innerText = inp.value; 
-})
